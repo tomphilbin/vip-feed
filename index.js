faa988295836a0015b920b16cd384a3bd5fc53c6
Christopher Kahn <chris.kahn@ynap.com>
2019-11-07 15:13:47 +0000

    CHanged port

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ setInterval(() => {
   console.log(JSON.stringify(highValueOrder));
 }, 5000);
 
-http.listen(3000, function() {
-  console.log("listening on *:3000");
+http.listen(3001, function() {
+  console.log("listening on *:3001");
 });
